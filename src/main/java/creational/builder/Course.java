87a6a4394e7ae461e36daf8642abbed72448dcf3
erlie <290059742@qq.com>
2018-11-05 23:38:04 +0800

    增加建造者模式

---
+++ src/main/java/creational/builder/Course.java
@@ -0,0 +1,21 @@
+package creational.builder;
+
+public class Course {
+
+    private String courseName;
+    private String coursePPT;
+    private String courseVideo;
+    private String courseArticle;
+
+    public static class CourseBuilder {
+
+        private String courseName;
+        private String coursePPT;
+        private String courseVideo;
+        private String courseArticle;
+
+    }
+
+
+
+}
